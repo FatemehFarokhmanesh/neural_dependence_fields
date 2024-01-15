fb9ea9fb1fad2cd1465db59afdabcefb3133c849
Christoph Neuhauser <c.a.neuhauser@gmail.com>
2024-01-15 13:18:06 +0100

    Added options for specifying file indices and variable name.

--- README.md
+++ README.md
@@ -10,6 +10,8 @@ tinycudann=1.7
 
 xarray=2023.1.0
 
+dask=2023.11.0
+
 Randomly sampling reference and query positions and computing groundtruths:
 ```
 conda activate env
--- mi_pearson_sampler.py
+++ mi_pearson_sampler.py
@@ -13,9 +13,11 @@ import pycoriander
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--source_path', type=str, default='/mnt/data2/ensemble/1000_member', help='path to ensemble dataset nc files')
-parser.add_argument('--num_train', type=int, default=1000000) 
+parser.add_argument('--indices', type=str, default=None, help='indices to substitute in \'--source_path\', e.g., for path \'file_%04i.nc\' it could be \'range(1,101)\'')
+parser.add_argument('--num_train', type=int, default=1000000)
 parser.add_argument('--num_validation', type=int, help='batch size', default=200000)
 parser.add_argument('--num_samples', type=int, help='resampling iteration', default=300)
+parser.add_argument('--varname', type=str, default='u', help='name of the variable to load from the .nc files')
 
 args_dict = vars(parser.parse_args())
 
@@ -27,6 +29,8 @@ multi_var = False
 # n_samples = 300
 
 SOURCE_PATH = args_dict['source_path']
+INDICES = args_dict['indices']
+VARNAME = args_dict['varname']
 NUM_POINTS_TRAIN = args_dict['num_train']
 NUM_POINTS_VAL = args_dict['num_validation']
 n_samples = args_dict['num_samples']
@@ -44,14 +48,41 @@ if not os.path.isdir(TRAIN_DIR):
 if not os.path.isdir(VALIDATION_DIR):
     os.makedirs(os.path.abspath(VALIDATION_DIR))
 
-members_path = sorted(
-    glob.glob(os.path.join(SOURCE_PATH, '*.nc')), reverse=False)
+if INDICES is not None:
+    start = 0
+    stop = None
+    step = 1
+    range_str = INDICES
+    range_str = range_str[range_str.find('(') + 1:range_str.find(')')]
+    range_str = range_str.split(',')
+    range_str = [s.strip() for s in range_str]
+    if len(range_str) == 1:
+        stop = int(range_str[0])
+    elif len(range_str) == 2:
+        start = int(range_str[0])
+        stop = int(range_str[1])
+    elif len(range_str) == 3:
+        start = int(range_str[0])
+        stop = int(range_str[1])
+        step = int(range_str[2])
+    else:
+        raise Exception('invalid indices string')
+    print(f'using range({start}, {stop}, {step})')
+    members_path = []
+    for i in range(start, stop, step):
+        members_path.append(SOURCE_PATH % i)
+else:
+    members_path = sorted(
+        glob.glob(os.path.join(SOURCE_PATH, '*.nc')), reverse=False)
 members_path = members_path[:MEMBERS]
 ensemble_data = xr.open_mfdataset(members_path,
                                   concat_dim='members', combine='nested')
-var_1 = ensemble_data.u.isel(time=5).values
+if 'time' in ensemble_data[VARNAME].dims:
+    var_1 = ensemble_data[VARNAME].isel(time=5).values
+else:
+    var_1 = ensemble_data[VARNAME].values
 if multi_var:
-    var_2 = ensemble_data.u.isel.values
+    var_2 = ensemble_data[VARNAME].isel.values
     variables = [var_1, var_2]
 # multivar
 if multi_var:
